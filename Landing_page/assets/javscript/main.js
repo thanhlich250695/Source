ce6296e3cf0ef7c62313104d1204b5560f3f0f51
tranthanhlich <thanhlich2506@gmail.com>
2020-02-20 00:36:39 +0700

    votraicay

--- Landing_page/assets/javscript/main.js
+++ Landing_page/assets/javscript/main.js
@@ -5,3 +5,50 @@ $(document).ready(function(){
     })
     
 })
+
+    $(".nav-1").click(function() {
+        $(".b-nav-tab").removeClass("active");
+        $(this).addClass("active");
+        $('#tab1').addClass('active')
+        $('#tab2').removeClass('active')
+        $('#tab3').removeClass('active')
+        $('#tab4').removeClass('active')
+        $('#tab5').removeClass('active')
+    });
+ 
+    $(".nav-2").click(function() {
+        $(".b-nav-tab").removeClass("active");
+        $(this).addClass("active");
+        $('#tab2').addClass('active')
+        $('#tab1').removeClass('active')
+        $('#tab3').removeClass('active')
+        $('#tab4').removeClass('active')
+        $('#tab5').removeClass('active')
+    });
+    $(".nav-3").click(function() {
+        $(".b-nav-tab").removeClass("active");
+        $(this).addClass("active");
+        $('#tab3').addClass('active')
+        $('#tab2').removeClass('active')
+        $('#tab1').removeClass('active')
+        $('#tab4').removeClass('active')
+        $('#tab5').removeClass('active')
+    });
+    $(".nav-4").click(function() {
+        $(".b-nav-tab").removeClass("active");
+        $(this).addClass("active");
+        $('#tab4').addClass('active')
+        $('#tab2').removeClass('active')
+        $('#tab3').removeClass('active')
+        $('#tab1').removeClass('active')
+        $('#tab5').removeClass('active')
+    });
+    $(".nav-5").click(function() {
+        $(".b-nav-tab").removeClass("active");
+        $(this).addClass("active");
+        $('#tab5').addClass('active')
+        $('#tab2').removeClass('active')
+        $('#tab3').removeClass('active')
+        $('#tab4').removeClass('active')
+        $('#tab1').removeClass('active')
+    });
