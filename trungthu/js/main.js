10ea3a2dce034193782e4b3dd881e6bc873d71af
TranThanhLich <lichtt@vng.com.vn>
2020-09-28 18:20:50 +0700

    aaa

--- trungthu/js/main.js
+++ trungthu/js/main.js
@@ -5,20 +5,19 @@ if(window.innerWidth > 981){
 	bg_video.play();
 
 	bg_video.addEventListener('ended', function () {
-		$(".brand").hide();
+		setTimeout(function(){ $(".brand").addClass('hide'); }, 24000);
 		bg_video.pause();
 	}, 0);
 }
 if(window.innerWidth < 980){
 	$('.button-action').click(function(){
 		$('.bl-login').fadeOut();
+		setTimeout(function(){ $(".brand").addClass('hide'); }, 24000);
 		$('.bl-video-m').fadeIn();
 		bg_video_m.play();
 	});
 	
 	bg_video_m.addEventListener('ended', function () {
-		console.log("complete");
-		$(".brand").hide();
 		bg_video_m.pause();
 	}, 0);
 }
